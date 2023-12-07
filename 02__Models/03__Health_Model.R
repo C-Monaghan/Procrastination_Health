f1eca0ff0af236e2b98d9f4cf79db2bddc47b644
C-Monaghan <cormacmonaghan@tutanota.com>
2023-12-07 15:47:50 +0000

    Running different models

--- 02__Models/03__Health_Model.R
+++ 02__Models/03__Health_Model.R
@@ -21,8 +21,29 @@ health_model <- '
     Procras_10 ~~ Procras_11; # don\'t get things done on time ~~ not  good at meeting deadlines
     
     # DIRECT EFFECT
-    Health_problems ~ c*pro;
+    Health_problems ~ c*pro + b1*Health_behaviours + b2*Total_stress;
+    
+    # A PATHS
+    Health_behaviours ~ a1*pro;
+    Total_stress ~ a2*pro;
+
+    
+    # TOTAL INDIRECT
+    HIDE := a1*b1;
+    SIDE := a2*b2;
+    
+    Total_IE := (a1*b1) + (a2*b2)
+    
+    # TOTAL EFFECT
+    Total_effect := c + (a1*b1) + (a2*b2)
 '
 
+# Fitting and reporting model --------------------------------------------------
 fit <- sem(health_model, data = health_data, estimator = "ML", missing = "fiml")
 summary(fit, fit.measures = TRUE, standardized = TRUE, modindices = FALSE, rsquare = TRUE)
+
+# Exporting --------------------------------------------------------------------
+export_path <- "./02__Models/Results/"
+
+output <- capture.output(summary(fit, fit.measures = TRUE, standardized = TRUE, modindices = FALSE, rsquare = TRUE))
+writeLines(output, file.path(export_path, "04__SEM_protection_stress.txt"))
