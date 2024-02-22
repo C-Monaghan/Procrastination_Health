0b3840818228985adac1bd53392468d7f4e2dd6d
C-Monaghan <cormacmonaghan@tutanota.com>
2024-02-22 10:19:45 +0000

    Updating GAM R code

--- 02__Models/Test Code/02__GAM.R
+++ 02__Models/Test Code/02__GAM.R
@@ -1,5 +1,6 @@
 rm(list = ls())
 
+library(dplyr)
 library(mgcv)
 library(sjPlot)
 library(patchwork)
@@ -9,9 +10,23 @@ path_data <- "./01__Data/02__Processed/"
 # Reading in data
 health_data <- readxl::read_xlsx(file.path(path_data, "Health_HRS.xlsx"))
 
+# Changing education to a factor
+health_data <- health_data %>%
+  mutate(Education_fac = factor(case_when(
+    Education == 0 ~ "No degree",
+    Education == 1 ~ "GED",
+    Education == 2 ~ "High School",
+    Education == 3 ~ "College (2yrs)",
+    Education == 4 ~ "College (4yrs)",
+    Education == 5 ~ "Masters",
+    Education == 6 ~ "Professional Degree"), 
+    levels = c("No degree", "GED", "High School", "College (2yrs)",
+               "College (4yrs)", "Masters", "Professional Degree")),
+    .after = Education)
+
 # Interaction Test -------------------------------------------------------------
 # fit <- glm(
-#   Dental_visit_2_years ~ (Total_procrastination + Total_depression + Age)^2 + Education,
+#   Dental_visit_2_years ~ (Total_procrastination + Total_depression + Age)^2 + Education_fac,
 #   data = health_data, family = "binomial")
 
 # summary(fit)
@@ -27,8 +42,8 @@ health_data <- readxl::read_xlsx(file.path(path_data, "Health_HRS.xlsx"))
 # Fitting a GAM ----------------------------------------------------------------
 # Prostate Exam
 fit_1 <- gam(
-  Prostate_exam ~ te(Total_procrastination, Total_depression) + Education + Age,
-  data = health_data, family = "binomial")
+  Prostate_exam ~ te(Total_procrastination, Total_depression) + Education_fac + s(Age),
+  data = health_data, family = "binomial", method = "REML")
 
 summary(fit_1)
 
@@ -37,8 +52,8 @@ plot(fit_1_residuals)
 
 # Pap Smear
 fit_2 <- gam(
-  Pap_smear ~ Total_procrastination + Education + te(Total_depression, Age),
-  data = health_data, family = "binomial")
+  Pap_smear ~ s(Total_procrastination) + Education + te(Total_depression, Age),
+  data = health_data, family = "binomial", method = "REML")
 
 summary(fit_2)
 
@@ -47,8 +62,8 @@ plot(fit_2_residuals)
 
 # Dentist
 fit_3 <- gam(
-  Dental_visit_2_years ~ te(Total_procrastination, Age) + Total_depression + Education,
-  data = health_data, family = "binomial")
+  Dental_visit_2_years ~ te(Total_procrastination, Age) + s(Total_depression, k = 9) + Education,
+  data = health_data, family = "binomial", method = "REML")
 
 summary(fit_3)
 
@@ -57,8 +72,8 @@ plot(fit_3_residuals)
 
 # Cholesterol Screening
 fit_4 <- gam(
-  Cholesterol_screening ~ Total_procrastination + Education + te(Total_depression, Age),
-  data = health_data, family = "binomial")
+  Cholesterol_screening ~ s(Total_procrastination) + Education + te(Total_depression, Age),
+  data = health_data, family = "binomial", method = "REML")
 
 summary(fit_4)
 
@@ -75,6 +90,10 @@ p1 <- wrap_elements(panel = ~ vis.gam(
   xlab = "Total Depression (0 - 8)", ylab = "Total Procrastination (0 - 60)", 
   zlab = "Predicted Probability (0 - 1)"
   ))
+  
+plot(fit_1, select = 2, trans = plogis, shift = coef(fit_1)[1], seWithMean = TRUE,
+     rug = FALSE, shade = TRUE, shade.col = "skyblue", ylim = c(0, 1), 
+     ylab = "Probability", main = "Probability of getting a prostate exam throughout age")
 
 # Pap Smear
 p2 <- wrap_elements(panel = ~ vis.gam(
@@ -119,4 +138,72 @@ cowplot::save_plot(filename = file.path(export_path, "04__Cholesterol_GAM.png"),
                    plot = p4, base_height = 10)
 
 
+library(dplyr)
+library(gratia)
+library(ggplot2)
 
+smooths(fit_1)
+
+x <- smooth_estimates(fit_1, smooth = "s(Age)")
+
+x %>%
+  add_confint() %>%
+  ggplot(aes(y = plogis(est), x = Age)) +
+  geom_ribbon(aes(ymin = plogis(lower_ci), ymax = plogis(upper_ci)), alpha = 0.2, fill = "skyblue") +
+  geom_line(colour = "black", linewidth = 0.74) +
+  labs(title = expression("Partial effect of" ~ f(Age)), 
+       y = "Partial effect", x = expression(Age)) +
+  ylim(0, 1) +
+  theme_bw() +
+  ggeasy::easy_center_title()
+
+
+test_fit <- gam(Heart_condition ~ s(Total_procrastination) + s(Total_depression, k = 9) + s(Age) + Education,
+                data = health_data, family = "binomial", method = "REML")
+
+summary(test_fit) 
+
+sim_res <- DHARMa::simulateResiduals(test_fit, n = 1000, seed = 150)
+plot(sim_res)
+
+
+
+
+
+
+test <- gam(Cholesterol ~ Education + s(Total_depression, k = 9) + te(Total_procrastination, Age), 
+            data = health_data, family = "binomial", method = "REML")
+
+summary(test)
+
+
+plot(effects::effect("Education_fac", y))
+
+
+
+y <- glm(Prostate_exam ~ Education_fac, 
+         data = health_data, family = binomial(link = "logit"))
+
+summary(y)
+
+pred <- data.frame(
+  Education_fac = health_data$Education_fac,
+  fit = predict(y, type = "response", newdata = health_data),
+  se = predict(y, type = "response", se.fit = TRUE, newdata = health_data)$se.fit)
+
+pred <- pred %>%
+  filter(complete.cases(fit))
+
+pred_lower <- pred$fit - 1.96 * pred$se
+pred_upper <- pred$fit + 1.96 * pred$se
+
+ggplot(data = pred, aes(Education_fac, y= fit, colour = Education_fac)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = pred_lower, ymax = pred_upper)) +
+  labs(title = "Predicted probability of getting a prostate exam by education level", 
+       x = "", y = "Predicted probability") +
+  ylim(0, 1.1) +
+  theme_classic() +
+  ggeasy::easy_remove_legend() +
+  ggeasy::easy_remove_gridlines() +
+  ggeasy::easy_center_title()
